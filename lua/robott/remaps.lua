4391a7cee7a2a83ada95e6cdcb01db793cee1ad7
robott11 <contato.robott@gmail.com>
2023-12-09 21:35:15 -0300

    feat: added cool remap that makes possible to replace words

--- lua/robott/remaps.lua
+++ lua/robott/remaps.lua
@@ -5,3 +5,4 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
