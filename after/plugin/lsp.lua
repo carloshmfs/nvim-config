c826a5143b8dd6a1b3070afccaf4e23faaaf68c2
robott11 <contato.robott@gmail.com>
2023-12-09 22:13:10 -0300

    feat: add remap keys to lsp zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,6 +9,15 @@ end)
 -- (Optional) Configure lua language server for neovim
 local lspconfig = require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
 lsp.set_sign_icons({
     error = '✘',
     warn = '▲',
