ebf12bdf1f3ba65563a4c64b24806e91a3ffae6e
robott11 <contato.robott@gmail.com>
2023-12-11 00:09:39 -0300

    feat: added new colorschemes

--- lua/robott/plugins/colorschemes.lua
+++ lua/robott/plugins/colorschemes.lua
@@ -3,5 +3,15 @@ return {
     { "ellisonleao/gruvbox.nvim", lazy = true },
 
     -- rosepine
-    { 'rose-pine/neovim', name = 'rose-pine' }
+    { 'rose-pine/neovim', name = 'rose-pine' },
+
+    -- tokyonight
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+
+    { "NLKNguyen/papercolor-theme" },
 }
