184144338ffa25f4965ee611704ff95cb8e025f4
robott11 <contato.robott@gmail.com>
2023-12-16 18:52:27 -0300

    feat: add catppuccin colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,4 +6,4 @@ function setColorScheme(color)
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-setColorScheme("rose-pine")
+setColorScheme("catppuccin")
--- lua/robott/plugins/colorschemes.lua
+++ lua/robott/plugins/colorschemes.lua
@@ -14,4 +14,23 @@ return {
     },
 
     { "NLKNguyen/papercolor-theme" },
+
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            transparent_background = true,
+            integrations = {
+                telescope = true,
+                harpoon = true,
+                mason = true,
+                neotest = true,
+            }
+        },
+        config = function(_, opts)
+            require('catppuccin').setup(opts)
+            vim.cmd.colorscheme 'catppuccin-mocha'
+        end
+    }
 }
