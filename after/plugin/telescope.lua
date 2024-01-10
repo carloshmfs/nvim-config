39dfcdcab25fc4a4cc5c8b158065484b3c781a5e
robott11 <contato.robott@gmail.com>
2024-01-09 23:36:14 -0300

    feat: add search in lsp symbols with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,7 @@ local builtin = require "telescope.builtin"
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>po", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<leader>ps", function ()
    builtin.grep_string({ search = vim.fn.input("Find in files: ") })
 end)
