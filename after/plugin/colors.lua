b989ae147979cd2359cb948d4f3dc6205bf6db22
carloshmfs <carloshmfs.dev@pm.me>
2024-04-10 23:20:49 -0300

    feat: add monokai-pro theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,14 @@
-function SetColorScheme(color)
+function color(color)
     color = color or "gruvbox"
+    if color == "monokai-pro" then
+        require("monokai-pro").setup({
+            filter = "spectrum"
+        })
+    end
 
     vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-SetColorScheme("moonfly")
+color("monokai-pro")
