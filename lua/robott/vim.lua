ff0b00573969d1053ba130f25c09c564532e050d
Robott11 <contato.robott@gmail.com>
2024-04-16 20:10:47 -0300

    feat: add vim configs

--- lua/robott/vim.lua
+++ lua/robott/vim.lua
@@ -21,7 +21,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
--- vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
 
@@ -31,3 +31,23 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 vim.opt.cursorline = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.updatetime = 250
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
