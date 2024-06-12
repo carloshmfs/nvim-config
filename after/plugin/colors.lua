fa8a654ad90a68ee72f714e59d25e5ce8b28011a
carloshmfs <carloshmfs.dev@pm.me>
2024-06-11 22:53:39 -0300

    feat(telescope): add transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,6 +2,10 @@ function color(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
 end
 
 color("dracula")
+
