85cde1b320ebded6e01289e346ed6f2b01fba2c4
carloshmfs <carloshmfs.dev@pm.me>
2024-10-26 15:52:55 -0300

    feat(colors): set the SignColumn background to transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,7 @@
 function color(color)
     vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
